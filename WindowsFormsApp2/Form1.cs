0fc0f11adaddf98965575129c92acac7757d82ee
Danila <Danila@DESKTOP-ECLVNJF>
2023-04-20 15:22:51 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        static int score = 0;
+        static int timer = 0;
+        static int timer_2 = 5;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            groupBox4.Text = "4";
+            radioButton6.Visible = true;
+            radioButton7.Visible = true;
+            label1.Text = "Осталось " + timer_2 + " сек";
+            label1.Visible = true;
+            timer2.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button2.Enabled = false;
+            if (radioButton2.Checked)
+                score += 5;
+            if (radioButton5.Checked)
+                score += 5;
+            if (radioButton9.Checked)
+                score += 10;
+            if (radioButton12.Checked)
+                score += 10;
+            if (radioButton13.Checked)
+                score += 10;
+            if (radioButton16.Checked)
+                score += 10;
+            if (radioButton19.Checked)
+                score += 10;
+            if (checkBox1.Checked && checkBox3.Checked)
+                score += 15;
+            if (checkBox4.Checked && checkBox6.Checked)
+                score += 15;
+            progressBar1.Value = score;
+            progressBar1.Visible = true;
+            string temp3 = score.ToString() + "%";
+            label3.Text = temp3;
+            label3.Visible = true;
+            label2.Visible = true;
+            Text = "Прошло " + timer + " сек";
+            string temp = "user";
+            int num = 1;
+            string file = File.ReadAllText("result.txt");
+            while (true)
+            {
+                if (!file.Contains(temp + num))
+                {
+                    StreamWriter sw = new StreamWriter("result.txt");
+                    sw.Write(file);
+                    sw.WriteLine(temp + num + " - " + score + " баллов за " + timer + " сек");
+                    sw.Close();
+                    break;
+                }
+                else
+                    num++;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer++;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (timer_2 == 0)
+            {
+                radioButton6.Enabled = false;
+                radioButton7.Enabled = false;
+                timer2.Stop();
+            }
+            else
+            {
+                timer_2--;
+                label1.Text = "Осталось " + timer_2 + " сек";
+            }
+        }
+    }
+}
